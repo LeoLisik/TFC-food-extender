20e6c5d8ff3c60331c9a9b68dcb7afef4da9340e
LeoLisik <trufelnaisveni@gmail.com>
2025-05-25 22:50:10 +0300

    Tested and updated

--- src/main/java/io/github/leolisik/tfcfoodextender/TFCFoodExtender.java
+++ src/main/java/io/github/leolisik/tfcfoodextender/TFCFoodExtender.java
@@ -20,47 +20,12 @@ import org.slf4j.Logger;
 @Mod(TFCFoodExtender.MODID)
 public class TFCFoodExtender
 {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "tfcfe";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-    //public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
-    //public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
-    //public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    //public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-    //public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
-
-    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
-
-    //public static final RegistryObject<Item> EXAMPLE_SEEDS = ITEMS.register("example_seeds",
-    //        () -> new Item(new Item.Properties()));
-
-    //public static final RegistryObject<Block> TEST_CROP = BLOCKS.register("test_crop",
-    //        () -> new CropBlock(BlockBehaviour.Properties.of().noCollission().instabreak().sound(SoundType.CROP)));
-
-
-
-    // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
-    /*public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            //.withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> TEST_FOOD.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(TEST_FOOD.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());*/
 
     @SuppressWarnings("removal")
     public TFCFoodExtender() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        //FEItems.ITEMS.register(modEventBus);
-        //FEBlocks.BLOCKS.register(modEventBus);
-        //FECreativeTabs.CREATIVE_TABS.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -68,34 +33,6 @@ public class TFCFoodExtender
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
-
-    // TODO: if runClient starts fine remove it
-    /*public ExampleMod(FMLJavaModLoadingContext context)
-    {
-        IEventBus modEventBus = context.getModEventBus();
-        FEItems.ITEMS.register(modEventBus);
-        FEBlocks.BLOCKS.register(modEventBus);
-        FECreativeTabs.CREATIVE_TABS.register(modEventBus);
-
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        //BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        //ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        //CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
-        //modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }*/
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
